215bce51883152c04d046c3b3bfa0b08b7d47485
Howon <howon.kim3667@gmail.com>
2025-10-24 23:18:56 +0900

    Add solution for a009

---
+++ a009_pyramid.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+
+
+int main(void){
+    //(1)
+    for(int i = 1; i <=5; i++){
+        for (int j = 1; j <=i; j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+    printf("\n");
+    //(2)
+    for(int i = 1; i <=5; i++){
+        for (int j = 1; j <= 2*i-1; j++ ){
+            printf("*");
+        }
+    }
+    printf("\n");
+    //(3)
+    for(int i = 5; i >=1; i--){
+        for (int j = 1; j <= i; j++ ){
+            printf("*");
+        }
+        printf("\n");
+    }
+    printf("\n");
+
+    //(4)
+    for(int i = 1; i <=5; i++){
+        for (int j = 1; j<=5-i; j++){
+            printf(" ");
+        }
+        for (int j = 1; j <= i; j++ ){
+            printf("*");
+        }
+        printf("\n");
+    }
+    printf("\n");
+
+    //(5)
+    for(int i = 1; i <=5; i++){
+        for (int j = 1; j<=5-i; j++){
+            printf(" ");
+        }
+        for (int j = 1; j <= 2*i-1; j++ ){
+            printf("*");
+        }
+        printf("\n");
+    }
+    printf("\n");
+
+    //(5)
+    for(int i = 5; i >=1; i--){
+        for (int j = 1; j<=5-i; j++){
+            printf(" ");
+        }
+        for (int j = 1; j <= 2*i-1; j++ ){
+            printf("*");
+        }
+        printf("\n");
+    }
+
+}
